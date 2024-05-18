aa06dfe34fdbbd034b4b696f966aa6bf49fdcd9e
Vakhtang Chanturidze <vakhtang92100@gmail.com>
2024-05-18 10:50:26 +0300

    Finish redux and table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,10 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers = []
+    this.storeSub = null
 
     this.prepare()
   }
@@ -26,6 +29,17 @@ export class ExcelComponent extends DomListener {
     const unsub = this.emitter.subscribe(event, fn)
 
     this.unsubscribers.push(unsub)
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  storeChanged() {
+  }
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   init() {
